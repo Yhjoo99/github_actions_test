4b402c3ab72196dfe0fd2cc6723d0ae548187c45
Yhjoo99 <yhjoo99@prephis.co.kr>
2025-12-19 18:00:42 +0900

    Refactor for multi-api support and add version check

---
+++ ServiceB/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceB.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
